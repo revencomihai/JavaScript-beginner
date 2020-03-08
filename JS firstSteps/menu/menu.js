99718fe4c562bb0acb1715c07d7d0430bb53cbb3
revencomihai <mihai.rev@gmail.com>
2020-03-08 15:14:37 +0200

    Update menu.js

--- JS firstSteps/menu/menu.js
+++ JS firstSteps/menu/menu.js
@@ -1,10 +1,13 @@
+//menu items prices
 var priceCola = 50;
 var priceSalad = 100;
 var priceSteak = 150;
 
+// menu itself
 var menu = "***\nCola - " + priceCola + "MDL\n***\nSteak - " + priceSalad + "MDL\n***\nSalad - " + priceSteak + "MDL\n***"
 alert(menu)
 
+//choosing how much we want to drink and eat
 var quantity = prompt('How many Colas?')
 var totalCost1 = quantity * priceCola;
 
@@ -14,6 +17,7 @@ var totalCost2 = quantity * priceSteak;
 var quantity = prompt('How many salads')
 var totalCost3 = quantity * priceSalad;
 
+//calculate the bill
 var sum = totalCost1 + totalCost2 + totalCost3;
 var bill = 'Your total is: ' + sum
 
